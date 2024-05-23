22afb6ea23e9077b3ad4c650330e1902e794bb47
FilippoBonanno <fbonanno15@gmail.com>
2024-05-23 17:17:57 +0200

    M-1

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
+  data() {
+    return {
+      
+      
+
+
+    }
+  },
+  methods: {
+    
+    
+
+    
+  },
+  mounted() {
+   
+    
+
+  }
+}
+</script>
+
+<template>
+  <div>header</div>
+  
+
+  <main>
+    main
+    
+  
+  </main>
+
+  <div>footer</div>
+      
+</template>
+
+<style scoped>
+
+</style>
